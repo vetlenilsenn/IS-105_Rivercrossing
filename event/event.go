4fb4f1bba15d295a1070022548aaa8114dbc6479
vetlenilsenn <vetle.nilsen@live.no>
2022-01-28 12:36:18 +0100

    event

---
+++ event/event.go
@@ -0,0 +1,15 @@
+var landVest = []string{"Korn", "Kylling", "Rev", "Korn"}
+var båt = []string{}
+var landØst = []string{}
+
+func landVest(){
+	return landVest
+}
+
+func båt(){
+	return båt
+}
+
+func landØst(){
+	return landØst
+}
