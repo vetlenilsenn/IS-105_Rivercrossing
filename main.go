08d2ae6e3aaff145575604ef19b0e65c7af9be3b
vetlenilsenn <vetle.nilsen@live.no>
2022-01-28 13:38:59 +0100

    Finito

--- main.go
+++ main.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	state "github.com/vetlenilsenn/IS-105_Rivercrossing/state"
+	"bufio"
+	"os"
+	"fmt"
 )
 
 
 func main() {
 
-	state.ViewState()
+	scanner := bufio.NewScanner(os.Stdin)
+
+
+	for {
+		
+		state.ViewState()
+		fmt.Println("Hva vil du sende over? --> kylling, rev eller korn")
+		scanner.Scan()
+		text := scanner.Text()
+		if(text != "kylling"){
+			fmt.Print("\033[H\033[2J")
+			fmt.Println("Du kan ikke sende over det!")
+			continue
+		}
+		state.PutIn()
+		state.ViewState()
+		state.Crossriver()
+		state.ViewState()
+		if(text == "kylling"){
+			break
+		}
+	}
+
 
 }
--- state/state.go
+++ state/state.go
@@ -5,9 +5,9 @@ import (
 	"strings"
 )
 
-var landVest = []string{"Korn", "Kylling", "Rev", "Korn"}
-var båt = []string{}
-var landØst = []string{}
+var landVest = []string{"Korn", "Kylling", "Rev",}
+var båt = []string{""}
+var landØst = []string{""}
 
 func ViewState() {
 	fmt.Print("LandVest: ")
@@ -16,4 +16,26 @@ func ViewState() {
 	fmt.Println(strings.Join(båt, " | "))
 	fmt.Print("LandØst: ")
 	fmt.Println(strings.Join(landØst, " | "))
+	fmt.Println("------------------------")
+}
+
+func PutIn() {
+
+	for i, v := range landVest {
+    if v == "Kylling" {
+        landVest = append(landVest[:i], landVest[i+1:]...)
+        break
+    }
+	}
+	båt[0] = båt[0] + "Kylling"
+}
+
+func Crossriver() {
+	for i, v := range båt {
+    if v == "Kylling" {
+        båt = append(båt[:i], båt[i+1:]...)
+        break
+    }
+	}
+	landØst[0] = landØst[0] + "Kylling"
 }
